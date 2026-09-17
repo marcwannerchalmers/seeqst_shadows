dc5ad94d38e31f9e76ffb18f6781806210ab789b
Marc Constantin Wanner <wanner@arrhenius1.hpc.arrhenius.naiss.se>
2026-09-17 15:11:05 +0200

    nothing

---
+++ run_majorana.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A naiss2026-4-1165-gpu
+#SBATCH -p gpu
+#SBATCH -G 1
+#SBATCH -n 1
+#SBATCH -t 05:00:00
+
+set -e
+
+cd "/nobackup/proj/disk/naiss2026-4-1165/personal/wanner/seeqst_shadows/"
+source .venv/bin/activate
+
+python -u run_experiment.py --config-dir=cfgs_arrhenius --config-name=arrhenius_majorana_batched.yaml
