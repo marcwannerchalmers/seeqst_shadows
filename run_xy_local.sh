7ba04337461256b69e44fb140be03d86193a162e
Marc Constantin Wanner <wanner@arrhenius1.hpc.arrhenius.naiss.se>
2026-09-18 08:57:22 +0200

    ran some experiments

--- run_xy_local.sh
+++ run_xy_local.sh
@@ -10,4 +10,4 @@ set -e
 cd "/nobackup/proj/disk/naiss2026-4-1165/personal/wanner/seeqst_shadows/"
 source .venv/bin/activate
 
-python -u run_experiment.py --config-dir=cfgs_arrhenius --config-name=arrhenius_xy_local_paulis.yaml
+python -u run_experiment.py --config-dir=cfgs_arrhenius --config-name=arrhenius_xy_local_paulis_manyobs.yaml
